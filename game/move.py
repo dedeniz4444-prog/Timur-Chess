7b297890a9596331c3c422cc9fe60e02eeb27547
dedeniz4444-prog <dedeniz4444@gmail.com>
2026-06-25 00:02:00 +0300

    Implement move_piece function with rules check

--- game/move.py
+++ game/move.py
@@ -1,1 +1,14 @@
+from game.rules import can_move_piece
 
+
+def move_piece(board, from_square, to_square):
+    if not can_move_piece(board, from_square, to_square):
+        return board, False
+
+    new_board = board.copy()
+    moving_piece = new_board[from_square]
+
+    new_board[to_square] = moving_piece
+    del new_board[from_square]
+
+    return new_board, True
